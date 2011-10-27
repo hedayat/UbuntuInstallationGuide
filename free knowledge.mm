5efa73ca5c06534b209cf49b83bf755f27c8dddb
Hedayat Vatankhah <hedayat.fwd@gmail.com>
2011-10-27 21:44:19 +0330

    Initial commit

---
+++ free knowledge.mm
@@ -0,0 +1,172 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1312443407216" ID="ID_1360387505" MODIFIED="1312443518491" TEXT="free knowledge">
+<node CREATED="1312443528754" ID="ID_1589571727" MODIFIED="1312443547515" POSITION="right" TEXT="free software">
+<node CREATED="1312443563218" ID="ID_1458100692" MODIFIED="1312443584761" TEXT="operating system">
+<node CREATED="1312443596358" ID="ID_900339027" MODIFIED="1312443608628" TEXT="ubuntu">
+<node CREATED="1312443613669" FOLDED="true" ID="ID_1808624120" MODIFIED="1316676285791" TEXT="installation">
+<node CREATED="1312443631337" ID="ID_1761263587" MODIFIED="1312443706032" TEXT="1- what things do I need?">
+<node CREATED="1312444066188" ID="ID_97555099" MODIFIED="1312444139253" TEXT="1- laptop,pc"/>
+<node CREATED="1312444144858" ID="ID_1488193452" MODIFIED="1312444178232" TEXT="2- installation cd"/>
+<node CREATED="1312444179958" ID="ID_1317073963" MODIFIED="1312444274697" TEXT="3- preferably internet"/>
+</node>
+<node CREATED="1312443710020" ID="ID_1898791061" MODIFIED="1312443804342" TEXT="2- what is the order of the usage of my tools?">
+<node CREATED="1312444292089" ID="ID_1968086955" MODIFIED="1312444370545" TEXT="1- putting the installation dvd in dvdrom"/>
+<node CREATED="1312444386424" ID="ID_1648610549" MODIFIED="1312444444761" TEXT="2- restarting or turning on the pc"/>
+</node>
+<node CREATED="1312443920696" ID="ID_1199534458" MODIFIED="1312443972986" TEXT="3- what do I need for partitioning?">
+<node CREATED="1312444495253" ID="ID_1687087073" MODIFIED="1312444531251" TEXT="1- partition editor tools"/>
+<node CREATED="1312444531995" ID="ID_1461747543" MODIFIED="1312444644609" TEXT="2- environment to do partitioning (for example live cd)"/>
+</node>
+<node CREATED="1312443977396" ID="ID_113884625" MODIFIED="1312444055777" TEXT="4- how can I find partition editor?">
+<node CREATED="1312444674192" ID="ID_881040055" MODIFIED="1312746019702" TEXT="1- partition editor is the administrating software = it&apos;s location is in administration"/>
+<node CREATED="1312444876514" ID="ID_1802357904" MODIFIED="1312444934325" TEXT="2-accesing the management software needs password"/>
+</node>
+<node CREATED="1313044936499" ID="ID_531247485" MODIFIED="1313044990951" TEXT="5- what can the partition editor software do?">
+<node CREATED="1313045010301" ID="ID_1780055683" MODIFIED="1313045083953" TEXT="1- creating partition">
+<node CREATED="1313045272530" ID="ID_416162915" MODIFIED="1313045353234" TEXT="1- is there enough space to create partition?">
+<node CREATED="1313045372067" ID="ID_1951972838" MODIFIED="1313045385390" TEXT="yes">
+<node CREATED="1313045413757" ID="ID_1264071655" MODIFIED="1313045522938" TEXT="what information do we need to make our partition?">
+<node CREATED="1313045832924" ID="ID_368841476" MODIFIED="1313045941610" TEXT="1- for what purpose do I need to create partition?"/>
+<node CREATED="1313045950258" ID="ID_1323803496" MODIFIED="1313045998012" TEXT="2- how much space will we need?"/>
+<node CREATED="1313046000384" ID="ID_514416903" MODIFIED="1313046045150" TEXT="3- what format does our partition need?"/>
+</node>
+</node>
+<node CREATED="1313045390106" ID="ID_553506101" MODIFIED="1313045395381" TEXT="no">
+<node CREATED="1313045536761" ID="ID_494525194" MODIFIED="1313045786673">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1- we refer to <b>deleting partition</b>
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1313045589064" ID="ID_1696646243" MODIFIED="1313045722247">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      2- after the above step, we go back to <b>creating partition</b>&#160;again
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1313045088364" ID="ID_163166076" MODIFIED="1313045126390" TEXT="2- deleting partition">
+<node CREATED="1313046083880" ID="ID_152722653" MODIFIED="1313046204541" TEXT="1- do we have any partition that we don&apos;t need it or the information in it?">
+<node CREATED="1313046299379" ID="ID_169260975" MODIFIED="1313046319328" TEXT="yes">
+<node CREATED="1313046333726" ID="ID_58060331" MODIFIED="1313046358407" TEXT="delete the partition"/>
+</node>
+<node CREATED="1313046321294" ID="ID_1003349799" MODIFIED="1313046324902" TEXT="no">
+<node CREATED="1313046363125" ID="ID_1449596105" MODIFIED="1313046436858" TEXT="1- we find the partition which has the least amount of info in it"/>
+<node CREATED="1313046452790" ID="ID_834955867" MODIFIED="1313046562622" TEXT="2- we find the partition which has enough space to transfer the above info"/>
+<node CREATED="1313046600209" ID="ID_269821613" MODIFIED="1313046786689" TEXT="3- we transfer the info from the targeted partition to a safe partitin in a common way"/>
+<node CREATED="1313046801781" ID="ID_1901441602" MODIFIED="1313046871308" TEXT="4- we delete the targeted partition"/>
+</node>
+</node>
+<node CREATED="1313046893531" ID="ID_745663155" MODIFIED="1313046989541" TEXT="2- we can continue deleting partitions as much as we like or necessary"/>
+</node>
+<node CREATED="1313045128929" ID="ID_1803111848" MODIFIED="1313045180487" TEXT="3- formating partition">
+<node CREATED="1313047061182" ID="ID_296382830" MODIFIED="1313047128458" TEXT="1- formating partitions will erase all the available info on the partition"/>
+<node CREATED="1313047145644" ID="ID_1510364451" MODIFIED="1313047276950" TEXT="2- formating partitions will not delete the partition itself"/>
+</node>
+<node CREATED="1313045182447" ID="ID_292487648" MODIFIED="1313045210778" TEXT="4- resizing partition">
+<node CREATED="1313047332160" ID="ID_888417768" MODIFIED="1313047556806" TEXT="1- for the purpose of increasing the space of a partition, we need an empty space alongside the partition"/>
+<node CREATED="1313047573595" ID="ID_323354426" MODIFIED="1313047764560" TEXT="2- for decreasing the space of a partition, it&apos;s obvious that the partition must have empty space"/>
+</node>
+</node>
+</node>
+<node CREATED="1313048124622" ID="ID_1602181368" MODIFIED="1313048229721" TEXT="general info about partitions">
+<node CREATED="1313048261697" FOLDED="true" ID="ID_1781192983" MODIFIED="1316676277770" TEXT="1- what is a partition?">
+<node CREATED="1313649508058" ID="ID_1598042720" MODIFIED="1314257469028" TEXT="A) Disk partitioning is the act of dividing a hard disk drive into multiple logical storage units referred to as partitions, to treat one physical disk drive as if it were multiple disks. Partitions are also termed &quot;slices&quot; for operating systems based on BSD, Solaris or GNU Hurd. A partition editor software program can be used to create, resize, delete, and manipulate these partitions on the hard disk."/>
+<node CREATED="1314257425437" ID="ID_533627681" MODIFIED="1314257453206" TEXT="B) In order to use the space in a hard disk, it must be partitioned. Partitioning is the process of dividing the hard disk&apos;s space into chunks, so they can be prepared for use, or even dedicated to different uses. Even if the entire disk is intended to be left in one piece, it must be partitioned so that the operating system knows that it is intended to be left in one piece. There are many different considerations that go into deciding how to partition a hard disk."/>
+</node>
+<node CREATED="1313048336786" FOLDED="true" ID="ID_1334179209" MODIFIED="1316676276048" TEXT="2- why do we make seperate partotions?">
+<node CREATED="1313653948674" ID="ID_283387187" MODIFIED="1314255290845" TEXT="because of needing different operating system we need different partitions"/>
+<node CREATED="1313651498486" ID="ID_1143798676" MODIFIED="1316675325479" TEXT="benefits">
+<font NAME="SansSerif" SIZE="15"/>
+<node CREATED="1313651671557" ID="ID_1680617399" MODIFIED="1313651671557" TEXT="Separation of the operating system (OS) and program files from user files. This allows image backups (or clones) to be made of only the operating system and installed software."/>
+<node CREATED="1313651704143" ID="ID_1193507499" MODIFIED="1316675517954" TEXT="Having a separate area for operating system virtual memory swapping/paging."/>
+<node CREATED="1313651732465" ID="ID_220276343" MODIFIED="1313651732465" TEXT="Keeping frequently used programs and data near each other."/>
+<node CREATED="1313651761877" ID="ID_1352831444" MODIFIED="1313651761877" TEXT="Having cache and log files separate from other files. These can change size dynamically and rapidly, potentially making a file system full."/>
+<node CREATED="1313651790556" ID="ID_1517586263" MODIFIED="1313651790556" TEXT="Use of multi-boot setups, which allow users to have more than one operating system on a single computer. For example, one could install Linux, BSD, Mac OS X, Microsoft Windows or other operating systems on different partitions of the same hard disk and have a choice of booting into any compatible operating system at power-up."/>
+<node CREATED="1313651815393" ID="ID_1859153705" MODIFIED="1316675331868" TEXT="Protecting or isolating files, to make it easier to recover a corrupted file system or operating system installation. If one partition is corrupted, other file systems may not be affected.">
+<font NAME="SansSerif" SIZE="13"/>
+</node>
+<node CREATED="1313651830535" ID="ID_399281145" MODIFIED="1313651830535" TEXT="Raising overall computer performance on systems where smaller file systems are more efficient. For instance, large hard drives with only one NTFS file system typically have a very large sequentially accessed Master File Table (MFT) and it generally takes more time to read this MFT than the smaller MFTs of smaller partitions."/>
+<node CREATED="1313651876321" ID="ID_1769895478" MODIFIED="1313651876321" TEXT="&quot;Short Stroking&quot;, which aims to minimize performance-eating head repositioning delays by reducing the number of tracks used per hard drive.[1] The basic idea is that you make one partition approx. 20-25% of the total size of the drive. This partition is expected to: occupy the outer tracks of the hard drive, and offer more than double the throughput &#x2014; less than half the access time. If you limit capacity with short stroking, the minimum throughput stays much closer to the maximum. This technique, however, is not related to creating multiple partitions, but generally just creating a partition lesser size than available on the disk.">
+<node CREATED="1313651876372" ID="ID_311828601" MODIFIED="1313651876372" TEXT="For example a 1 TB disk might have an access time of 12 ms at 200 IOPS (at a limited queue depth) with an average throughput of 100 MB/s. When it is partitioned to 100 GB (and the rest left unallocated) you might end up with an access time of 6 ms at 300 IOPS (with a bigger queue depth) with an average throughput of 200 MB/s."/>
+</node>
+</node>
+<node CREATED="1313651512411" ID="ID_860993599" MODIFIED="1313651532835" TEXT="disadvantages">
+<node CREATED="1313653830567" ID="ID_91220890" MODIFIED="1313653830567" TEXT="Reduces the total space available for user storage on the disk, as it forces the operating system to duplicate certain file system administration areas on the disk for each partition."/>
+<node CREATED="1313653850365" ID="ID_1638148465" MODIFIED="1313653850365" TEXT="May prevent using the whole disk capacity, because it may break free capacities apart. For example, if you have a disk with two partitions, each with 3 GBs free (hence 6GBs in total), you can&apos;t copy a 4GB DVD image file on that disk, because none of the partitions will actually provide enough space for that - even though you have more than enough free capacity in total on the disk. If the same files on those two partitions would have been stored on a single partition spanning the whole disk, then the 4GB file could be easily stored in the 6GB of free space."/>
+<node CREATED="1313653870650" ID="ID_1965511264" MODIFIED="1313653870650" TEXT="Slows down moving data between different parts of the same physical disk. When moving data from one partition to other, the operating system actually has to copy the contents of the data file, even though it still remains on the same device in a single copy/instance. When using a single partition, moving data between directories will only require altering the file system administration areas, and the actual contents of the file will not be copied or moved inside the disk/device, thus resulting in an indefinitely faster completion of the operation."/>
+</node>
+</node>
+<node CREATED="1313048533312" ID="ID_1829337740" MODIFIED="1317281964606" TEXT="3- what important information do we need to create partitions?">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1314255690162" ID="ID_284233948" MODIFIED="1314255818684" TEXT="what are the different kinds of partition?">
+<node CREATED="1314256416363" ID="ID_1808988957" MODIFIED="1314256439457" TEXT="primary partitions">
+<node CREATED="1314256497471" ID="ID_1751676875" MODIFIED="1314256545656" TEXT="what is a primary partition?">
+<node CREATED="1314257598953" ID="ID_574470301" MODIFIED="1314257598953" TEXT="A maximum of four partitions can be placed on any hard disk. These are sometimes called primary partitions. The limitation of four is one that is imposed on the system by the way that the master boot record is structured."/>
+</node>
+<node CREATED="1314256570573" ID="ID_560566827" MODIFIED="1314256603024" TEXT="important points:">
+<node CREATED="1314257611918" ID="ID_329736241" MODIFIED="1314258056433" TEXT="1- There are at the most four primary partitions"/>
+<node CREATED="1314257732046" ID="ID_1736867765" MODIFIED="1314257769476" TEXT="2- Only one partition may be designated, at any given time, as active. That partition will be used for booting the system."/>
+<node CREATED="1314257824218" ID="ID_1215173645" MODIFIED="1314257872241" TEXT="3- windows can only be installed in primary partitions"/>
+</node>
+</node>
+<node CREATED="1314256439927" ID="ID_876224449" MODIFIED="1314256456570" TEXT="extended partitions">
+<node CREATED="1314256615507" ID="ID_704172697" MODIFIED="1314256641748" TEXT="what is an extended partition?">
+<node CREATED="1314258317251" ID="ID_497263702" MODIFIED="1314258364898" TEXT="One of the four partitions may be designated as an extended partition. This partition may then be subdivided into multiple logical partitions. This is the way that two or more logical volumes can be placed on a single hard disk."/>
+</node>
+<node CREATED="1314256667238" ID="ID_1068949292" MODIFIED="1314256683034" TEXT="important points:">
+<node CREATED="1314258417690" ID="ID_1580318382" MODIFIED="1314258517505" TEXT="1- any time we make an extended partition it will be the last primary partition"/>
+<node CREATED="1314258517590" ID="ID_1860037708" MODIFIED="1314258704911" TEXT="2- an extended partition can be subdivided into multiple(countless) partitions"/>
+<node CREATED="1314258890686" ID="ID_553064710" MODIFIED="1314258940834" TEXT="3- each extended partition is at least one logical partition"/>
+<node CREATED="1314258709791" ID="ID_107132230" MODIFIED="1314258959420" TEXT="4- free OS can also be installed in extended(logical) partitions"/>
+</node>
+</node>
+</node>
+<node CREATED="1314255881122" ID="ID_75877391" MODIFIED="1314256028762" TEXT="what are the essentials of partitions?">
+<node CREATED="1316677547019" ID="ID_1680670395" MODIFIED="1316677559693" TEXT="space">
+<node CREATED="1316678563879" ID="ID_524479884" MODIFIED="1316678749094" TEXT="to make a partition, we need a hard with free space"/>
+</node>
+<node CREATED="1316677578013" ID="ID_774113428" MODIFIED="1316678538091" TEXT="File System or Format(substance)">
+<node CREATED="1316681172682" ID="ID_930220647" MODIFIED="1316681735166" TEXT="what is a file system">
+<node CREATED="1316680596714" ID="ID_548151451" MODIFIED="1316681196862" TEXT="A file system (filesystem) is a means to organize data expected to be retained after a program terminates by providing procedures to store, retrieve and update data, as well as manage the available space on the device(s) which contain it. A file system organizes data in an efficient manner and is tuned to the specific characteristics of the device. There is usually a tight coupling between the operating system and the file system. Some filesystems provide mechanisms to control access to the data and metadata. Ensuring reliability is a major responsibility of a filesystem. Some filesystems provide a means for multiple programs to update data in the same file at nearly the same time.  Without a filesystem programs would not be able to access data by file name or directory and would need to be able to directly access data regions on a storage device."/>
+<node CREATED="1316681735166" ID="ID_1278897187" MODIFIED="1316681833657" TEXT="file system is a tool for comunications between hard disk and OS"/>
+</node>
+<node CREATED="1316681843742" ID="ID_1040459699" MODIFIED="1316681865647" TEXT="function of file system">
+<node CREATED="1316681893725" ID="ID_998598961" MODIFIED="1316682020766" TEXT="access of OS to stored data on hard disk with special abilities"/>
+</node>
+<node CREATED="1316682062710" ID="ID_1994379574" MODIFIED="1316682089905" TEXT="some of the abilities of file system">
+<node CREATED="1316682093327" ID="ID_1236564734" MODIFIED="1316682230460" TEXT="* storing at the same time"/>
+<node CREATED="1316682231232" ID="ID_27132415" MODIFIED="1316682277699" TEXT="* access from network"/>
+<node CREATED="1316682291559" ID="ID_835456968" MODIFIED="1316682324962" TEXT="* the speed of reading data"/>
+<node CREATED="1316682329660" ID="ID_845170574" MODIFIED="1316682463487" TEXT="* some file systems are made for recovery"/>
+<node CREATED="1316682505504" ID="ID_1962274219" MODIFIED="1316682530891" TEXT="* searching ability"/>
+</node>
+</node>
+<node CREATED="1316677714168" ID="ID_1831534088" MODIFIED="1316677733387" TEXT="kind of partition"/>
+</node>
+<node CREATED="1314256067916" ID="ID_1884097884" MODIFIED="1314259177413" TEXT="what is the scheme of partitioning for each OS?"/>
+</node>
+<node CREATED="1313048414107" ID="ID_406662715" MODIFIED="1314255397026" TEXT="4- how do we make different partitions?"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
